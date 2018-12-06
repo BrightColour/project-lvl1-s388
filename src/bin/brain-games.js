d1210c0aadab9274ea7ef1b6501e3e5fc2643677
BrightColour <bright_color@bk.ru>
2018-12-07 01:10:34 +0300

    добавила интерфейс

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
-import brainGame from '..';
+import gameIntarface from '..';
 
-brainGame();
+const answers = {
+  no: 'no',
+  yes: 'yes',
+};
+
+const startMessage = `Welcome to the Brain Games!\nAnswer "${answers.yes}" if number even otherwise answer "${answers.no}".`;
+
+const getQuestionAndCorrectAnswer = () => {
+  const randomNumber = Math.floor(Math.random() * 100);
+  return {
+    question: randomNumber,
+    correctAnswer: (randomNumber % 2 === 0) ? answers.yes : answers.no,
+  };
+};
+
+const parseAnswerFormat = answer => Object.keys(answers).includes(answer);
+
+const evalGame = gameIntarface(getQuestionAndCorrectAnswer, parseAnswerFormat, startMessage);
+
+evalGame();
